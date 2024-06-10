a9dd6fcbcffdf57df56ee3b4cf5729db7e5cd574
mark <1464089197@qq.com>
2024-06-11 00:16:23 +0800

    web静态文件集成

--- user-end/src/main.rs
+++ user-end/src/main.rs
@@ -1,16 +1,20 @@
-use actix_web::{web, App, HttpServer, Responder};
 
-async fn greet() -> impl Responder {
-    "Hello, world!"
-}
+// 中间件
+// 统计耗时
+// 处理静态文件
+// 日志
+
+mod web_dist;
+
+use actix_web::{App, HttpServer};
+use crate::web_dist::{dist, index};
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-    })
-        .bind("127.0.0.1:8080")?
-        .run()
+async fn main() ->std::io::Result<()> {
+    HttpServer::new(|| App::new()
+        .service(index)
+        .service(dist))
+        .bind("127.0.0.1:8000")
+    ?.run()
         .await
 }
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(()=>{
+  return {
+    base: 'web', // 和部署有关系 url访问时带的base路径
+    plugins: [react()],
+  }
 })
