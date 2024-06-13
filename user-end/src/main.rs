c9fe19425cc339d7c96a8f0e82282a0b747887a9
mark <1464089197@qq.com>
2024-06-13 23:57:33 +0800

    sync

--- user-end/src/main.rs
+++ user-end/src/main.rs
@@ -3,41 +3,11 @@
 // 处理静态文件
 // 日志
 
-// 动态是否console窗口
 // 启动浏览器
 // tray图标
 
-// option result使用   str string区别    单例使用   expect、问号使用\unwrap
-
 // 写在main.rs的第一行 指示编译器生成GUI程序而不是console程序 这样编译出来的windows程序就不会弹出黑框
-// #![windows_subsystem = "windows"]
-
-// 根据不同的平台选择编译
-// #[cfg(target_os = "windows")]
-// {
-// windows_specific_function();
-// }
-//
-// #[cfg(target_os = "linux")]
-// {
-// linux_specific_function();
-// }
-//
-// #[cfg(target_os = "macos")]
-// {
-// macos_specific_function();
-// }
-
-// 根据debug和release选择编译
-// #[cfg(debug_assertions)]
-// {
-// println!("Running in debug mode");
-// }
-//
-// #[cfg(not(debug_assertions))]
-// {
-// println!("Running in release mode");
-// }
+#![windows_subsystem = "windows"]
 
 use std::path::Path;
 
@@ -60,11 +30,7 @@ mod config;
 async fn main() -> std::io::Result<()> {
 
     // 创建一个 TrayItem 对象，并设置图标和标题
-    let mut tray = TrayItem::new("Tray Example", IconSource::Data {
-        height: 0,
-        width: 0,
-        data: vec![],
-    }).unwrap();
+    let mut tray = TrayItem::new("Tray Example", IconSource::RawIcon()).unwrap();
 
     // 创建菜单项
     tray.add_menu_item("Show Message", || {
